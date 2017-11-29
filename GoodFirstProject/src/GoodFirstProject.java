35f3c0972ec26f604f0171c335974be26a88e007
darianrezania <darianrezania@gmail.com>
2017-11-29 11:49:26 -0800

    GoodFirstProject
    nothing

---
+++ GoodFirstProject/src/GoodFirstProject.java
@@ -0,0 +1,16 @@
+
+public class GoodFirstProject 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello Again");
+		System.out.println("I like typing this.");
+		System.out.println("This fun.");
+		System.out.println("Yay printing");
+		System.out.println("I'd muh rather you 'not'.");
+		System.out.println("I \"said\" do not touch this.");
+		
+	}
+
+}

9648891d296f2d6a50e5851041086eac33aa344c
darianrezania <darianrezania@gmail.com>
2017-12-01 11:09:58 -0800

    YourSchedule

---
+++ Your Schedule.java/src/YourSchedule.java
@@ -0,0 +1,23 @@
+
+public class YourSchedule 
+{
+	public static void main ( String[] args)
+	{
+		String myMath, myEnglish,myBiology, myECS, Myers, Farrar, Eddingfield, Mauro;
+		myMath= "|Farrar|";
+		myBiology= "|Eddingfield|";
+		myECS= "|Mauro|";
+		myEnglish="|Myers|";
+		Farrar= myMath;
+		Eddingfield=myBiology;
+		Mauro=myECS;
+		Myers=myEnglish;
+		
+		
+		System.out.println("|Per 1| " + myBiology+ Eddingfield);
+		System.out.println("|Per 2| " +  myECS + Mauro );
+		System.out.println("|Per 3| " +  myMath + Farrar);
+		System.out.println("|Per 4| " +  myEnglish + Myers);
+	}
+
+}

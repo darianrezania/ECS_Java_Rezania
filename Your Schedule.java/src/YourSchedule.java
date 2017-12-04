d7345c09ace249623a33bea22732c83ed11fda92
darianrezania <darianrezania@gmail.com>
2017-12-04 10:23:25 -0800

    Keyboard Inputs  AgeIn5Java

--- Your Schedule.java/src/YourSchedule.java
+++ Your Schedule.java/src/YourSchedule.java
@@ -4,11 +4,10 @@ public class YourSchedule
 	public static void main ( String[] args)
 	{
 		String myMath, myEnglish,myBiology, myECS, Myers, Farrar, Eddingfield, Mauro;
-		myMath=    "|     Farrar       |";
-		myBiology= "|     Eddingfield  |";
-		myEnglish= "|     Myers        |";
-		myECS=     "|     Mauro        |";
-		
+		myMath=    "| Math   |    Farrar       |";
+		myBiology= "| Biology|    Eddingfield  |";
+		myEnglish= "| English|    Myers        |";
+		myECS=     "| ECS    |     Mauro       |";
 		System.out.println("|Per 1| " + myBiology);
 		System.out.println("|Per 2| " +  myECS);
 		System.out.println("|Per 3| " +  myMath);

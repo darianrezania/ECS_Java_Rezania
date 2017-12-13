ea9c0a21214f74d9a5cdb610518324c25bbdfd8e
darianrezania <darianrezania@gmail.com>
2017-12-13 11:18:57 -0800

    CollatzSequence

--- HiLowLimited/src/HiLowLimited.java
+++ HiLowLimited/src/HiLowLimited.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 
 public class HiLowLimited
 {
-	public static void main ( String[] args )
+	public static void main( String[] args )
 	{
 		Scanner keyboard = new Scanner(System.in);
 		Random r = new Random();
 		int tries=1;
 		int randomNum = 1 + r.nextInt(100);
 		int guess = keyboard.nextInt();
-		String response = "";
 		
 		System.out.println("I'm thinking of a number from 1 to 100. Try to guess it.");
 		int choice = keyboard.nextInt();
 		
 		
 		
-		while(guess != randomNum && tries < 7)
+		while(choice != randomNum && tries < 7)
 		{
-			if (randomNum > guess)
+			if (randomNum > choice)
 			{
 				System.out.println("It is to low, guess again.");
 				randomNum = keyboard.nextInt();
@@ -45,3 +44,4 @@ public class HiLowLimited
 		
 	}
 }
+
---
+++ HiLowLimited2/src/HiLowLimited2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Random;
+public class HiLowLimited2
+{
+    
+    public static void main( String[] args ) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        Random r = new Random();
+        
+        int number = 1 + r.nextInt(100);
+        
+        System.out.println( "I'm thinking of a number between 1-100. You have 7 guesses." );
+        System.out.print( "Guess#1: " );
+        int guess = keyboard.nextInt();
+        int tries = 1;
+        
+        while ( guess != number && tries < 7 ) {
+            if ( guess > number ) {
+                System.out.println( "Sorry, your guess is too high." );
+            }
+            
+            if ( guess < number ) {
+                System.out.println( "Sorry, your guess is too low." );
+            }
+            
+            tries++;
+            
+            System.out.print( "Guess #" + tries + ":" );
+            guess = keyboard.nextInt();
+        }
+        
+        if ( guess != number ) {
+            System.out.println( "Sorry, you didn't guess it in 7 tries. You lose." );
+        }
+        
+        if ( guess == number ) {
+            System.out.println( "Wow, you got it in only " + tries + " tries! What are the odds!" );
+        }
+        
+    }
+}
